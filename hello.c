64e590fb6bd7fcc79f015efad0754a6d0aa10981
cwl2019 <49391337+cwl2019@users.noreply.github.com>
2019-04-09 10:19:13 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
 int main(){
-	printf ("hello,github");
+	char name[16];
+	printf ("u name:");
+	scanf (" %15s",name);
+	
+	printf ("%s,hello,github\n",name);
+	char *p=(char*)memalloc(sizeof(char) * 1024);
+	if(p){
+		printf ("allocate pointer ok.%0x",p);
+		memcpy(p,"hello,world");
+		printf ("%s\n",p);
+		free(p);
+	}
 	return 0;
 }
